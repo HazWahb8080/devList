e6add479e2defe719e9cc4fb74925b1351c6d6b9
HazWahb8080 <hazem.wahb100@gmail.com>
2022-09-29 20:54:28 +0200

    adding github login provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter"
 
 
@@ -11,6 +12,10 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
     // ...add more providers here
   ],
   adapter: FirestoreAdapter({
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,22 +1,38 @@
-import { getProviders, signIn } from "next-auth/react"
+/* eslint-disable @next/next/no-img-element */
+import { getProviders, signIn } from "next-auth/react";
+import Image from "next/image";
 
 export default function SignIn({ providers }) {
   return (
-    <>
+    <div className="bg-gradient-to-b from-transparent to-[#FED7B8] items-center justify-start flex flex-col min-h-screen ">
+      <img
+        src="/devList.png"
+        className="object-center object-cover mt-12"
+        height={100}
+        width={200}
+        alt="logo"
+      />
+      <div className="mb-24 "/>
+      <div className="items-center justify-start flex flex-col w-1/3 py-12">
+        <h1 className="mb-12 text-xl"> signIn or sign Up Below </h1>
       {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id,{callbackUrl:"/"})}>
+        <div key={provider.name} className="z-10">
+          <button
+            className="btn"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
             Sign in with {provider.name}
           </button>
         </div>
       ))}
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-  const providers = await getProviders()
+  const providers = await getProviders();
   return {
     props: { providers },
-  }
+  };
 }
