5df02a0afcc817c83aac2f53a2e13bf06eba70bc
HazWahb8080 <hazem.wahb100@gmail.com>
2022-09-29 21:25:28 +0200

    signin complete

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { getProviders, signIn } from "next-auth/react";
 import Image from "next/image";
-
+import { githubIcon, googleIcon } from "../../utils/svgs";
 export default function SignIn({ providers }) {
   return (
     <div className="bg-gradient-to-b from-transparent to-[#FED7B8] items-center justify-start flex flex-col min-h-screen ">
@@ -9,22 +9,28 @@ export default function SignIn({ providers }) {
         src="/devList.png"
         className="object-center object-cover mt-12"
         height={100}
-        width={200}
+        width={150}
         alt="logo"
       />
-      <div className="mb-24 "/>
+      <div className="mb-24 " />
       <div className="items-center justify-start flex flex-col w-1/3 py-12">
         <h1 className="mb-12 text-xl"> signIn or sign Up Below </h1>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name} className="z-10">
-          <button
-            className="btn"
-            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+        {Object.values(providers).map((provider) => (
+          <div
+            key={provider.name}
+            className="items-center justify-center flex flex-col"
           >
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
+            <button
+              className="btn my-4 flex space-x-4"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              <span className="self-center mx-2 ">
+                {provider.name === "Google" ? googleIcon : githubIcon}
+              </span>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
